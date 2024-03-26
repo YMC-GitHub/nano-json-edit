a020670f93a119a066ab0b5173137299738d837e
yemiancheng <ymc.github@gmail.com>
2024-03-26 11:57:10 +0800

    build(core): use vite-plugin-nano to min and add banner

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
+
+/* eslint-disable prefer-const */
+
 import fs from 'node:fs'
 import path from 'node:path'
 import { spawn } from 'node:child_process'
 import { builtinModules } from 'node:module'
 import { defineConfig } from 'vite'
 // import plainText from 'vite-plugin-plain-text';
-import banner from 'vite-plugin-banner'
+// import banner from 'vite-plugin-banner'
 import eslint from "vite-plugin-eslint";
+import nanoViteMiniDist from "vite-plugin-nano"
 
 import pkg from './package.json'
 // import fg from 'fast-glob';
-
+// prefer-const
 const isDevEnv = process.argv.slice(2).includes('--watch')
 const jsfileOutDir: string = "dist"
 const tsTypeOutDir: string = "types"
@@ -21,9 +25,7 @@ const jsfileSrcDir='lib'
 // console.log(tsentryfiles)
 // const {log}=console
 
-
-// 'min'
-
+let bannerText = `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`
 let  plugins =[
   isDevEnv?eslint({ lintOnStart: true, cache: false }):undefined,
   // allow all *.md files can be import as es module
@@ -32,52 +34,17 @@ let  plugins =[
       name: 'generate-types',
       async closeBundle() {
           if (process.env.NODE_ENV === 'test') return
-
           removeTypes()
           await generateTypes()
           moveTypesToDist()
           removeTypes()
       },
   },
-  banner(
-    `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * homepage: ${pkg.homepage}\n */`
-  )
+  // banner(bannerText),
+  nanoViteMiniDist({banner:bannerText})
 ]
+// console.log(stdUmdName(pkg.name))
 
-export default defineConfig({
-    build: {
-        minify: false,
-        outDir: jsfileOutDir,
-        emptyOutDir: !isDevEnv,
-        // target: 'node14',
-        lib: {
-            entry: [jsfileSrcDir,'main.ts'].join("/"),
-            // formats: ['cjs', 'es','umd'],
-            name: stdUmdName(pkg.name),
-            formats: ['cjs', 'es','umd'],
-            // fileName: format => format === 'es' ? '[name].mjs' : '[name].js',
-            fileName: format => format === 'es' ? '[name].js' : format === 'umd' ?'[name].umd.cjs':'[name].cjs',
-        },
-        rollupOptions: {
-            external: [
-                'vite',
-                ...builtinModules,
-                ...builtinModules.map(m => `node:${m}`),
-                ...Object.keys('dependencies' in pkg ? pkg.dependencies as object : {}),
-            ],
-            output: {
-                exports: 'named',
-            },
-        },
-    },
-    plugins: plugins.filter(v=>v)
-})
-
-// eslint-disable-next-line
-function makeLibFileName(format){
-  const res =  format === 'es' ? '[name].js' : format === 'umd' ?'[name].umd.cjs':'[name].cjs'
-  return res
-}
 
 function removeTypes() {
     console.log(`[types] declaration remove`)
@@ -100,15 +67,15 @@ function generateTypes() {
 }
 
 function moveTypesToDist() {
-  let types = path.join(__dirname, tsTypeOutDir,jsfileSrcDir)
-  const dist = path.join(__dirname, jsfileOutDir)
-  // use types when types/lib not exsits for file in src  disable -> types/src not exist 
-  // if(!fs.existsSync(types)) types = path.join(__dirname, tsTypeOutDir)
-  if(!fs.existsSync(types)) return
-  let files = fs.readdirSync(types).filter(n => n.endsWith('.d.ts'))
+    let types = path.join(__dirname, tsTypeOutDir,jsfileSrcDir)
+    const dist = path.join(__dirname, jsfileOutDir)
+    // use types when types/lib not exsits for file in src  disable -> types/src not exist 
+    // if(!fs.existsSync(types)) types = path.join(__dirname, tsTypeOutDir)
+    if(!fs.existsSync(types)) return
+    let files = fs.readdirSync(types).filter(n => n.endsWith('.d.ts'))
 
-  // ignore test files
-  files = files.filter(name=> !name.endsWith('.test.d.ts'))
+    // ignore test files
+    files = files.filter(name=> !name.endsWith('.test.d.ts'))
     for (const file of files) {
         fs.copyFileSync(path.join(types, file), path.join(dist, file))
         console.log('[types]', `${tsTypeOutDir}/${file} -> ${jsfileOutDir}/${file}`)
@@ -118,3 +85,39 @@ function moveTypesToDist() {
 function stdUmdName(name:string){
   return name.replace(/'@.*\/'/ig,'').replace(/-/ig,'')
 }
+
+
+export default defineConfig({
+  build: {
+      // terserOptions: {
+      //     compress: {
+      //         drop_console: true,
+      //         drop_debugger: true,
+      //     },
+      // },
+      // minify: !isDevEnv?'terser':false,
+      minify:false,
+      outDir: jsfileOutDir,
+      emptyOutDir: !isDevEnv,
+      // target: 'node14',
+      lib: {
+          entry: [jsfileSrcDir,'main.ts'].join("/"),
+          name: stdUmdName(pkg.name),
+          formats: ['cjs', 'es','umd'],
+          // fileName: format => format === 'es' ? '[name].mjs' : '[name].js',
+          fileName: format => format === 'es' ? '[name].js' : format === 'umd' ?'[name].umd.cjs':'[name].cjs',
+      },
+      rollupOptions: {
+          external: [
+              'vite',
+              ...builtinModules,
+              ...builtinModules.map(m => `node:${m}`),
+              ...Object.keys('dependencies' in pkg ? pkg.dependencies as object : {}),
+          ],
+          output: {
+              exports: 'named',
+          },
+      },
+  },
+  plugins: plugins.filter(v=>v),
+})
